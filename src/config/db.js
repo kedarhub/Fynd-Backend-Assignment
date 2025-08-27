c0abb60d89dd410ee17b95392b305c64c8664719
Kedar Singh <115499163+kedarhub@users.noreply.github.com>
2025-08-27 19:56:12 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://aman:s2o7tTWc0GyQTOC9676@cluster0.mntszxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("");
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
